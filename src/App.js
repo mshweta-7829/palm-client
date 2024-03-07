bf7b690fa218cd333864b979b7216fb23ed04cb5
Shweta Hosamani <mshweta.7829@gmail.com>
2024-03-07 11:31:32 -0800

    Add chatbot client code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async(e) => {
+    setLoading(true);
+    e.preventDefault();
+    const resp = await fetch("http://localhost:8000/api", {
+      method : 'POST',
+      headers : {
+      Accept : 'application/json',
+      'Content-Type' : 'application/json' 
+      }, 
+      body : JSON.stringify({
+        prompt 
+      })
+    })
+
+    const res = await resp.json();
+    setResponse(res);
+    setPrompt("");
+    setLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1 className="App-header">
+        ChatBot
+      </h1>
+      <div style={{margin:'0', flexGrow:'1', overflow:'scroll'}}>
+        <div style={{width:'100%', height:'100%'}}>
+          {loading && <h2>Loading</h2>}
+        {!loading && <article style={{color:"white", margin:'0'}}>{response}</article>}
+        </div>
+      </div>
+      <div className='footer'>
+        <textarea className='prompt' placeholder="Ask me anything" value={prompt} onChange={(e) => setPrompt(e.target.value)}></textarea>
+        <button className='btn' onClick={handleSubmit}>Go</button>
+        </div>
+    </main>
   );
 }
 
